d4e9b8cc9ff5eefaea599930904cd3596227279a
Matthew <mwhiteman83@gmail.com>
2022-07-16 13:33:45 -0600

    added switch case to take in employee methods based on inq answer.

--- server.js
+++ server.js
@@ -18,5 +18,54 @@ firstQuestion();
 
 function firstQuestion() {
 
+    inquirer
+    .prompt({
+      type: "list",
+      name: "operation",
+      message: "Would you like to accomplish?",
+      choices: [
+        "View Employees",
+        "View Departments",
+        "View Roles",
+        "Add new Employee",
+        "Update Employee Role",
+        "Add Role",
+        "Add new Department",
+        "End"]
+    })
+    .then(function ({ operation }) {
+        switch (operation) {
+          case "View Employees":
+            viewEmployee();
+            break;
+  
+          case "View Departments":
+            viewByDepartment();
+            break;
+        
+          case "View Roles":
+            viewRoles();
+            break;
+  
+          case "Add new Employee":
+            addEmployee();
+            break;
+  
+          case "Update Employee Role":
+            updateEmployee();
+            break;
+  
+          case "Add Role":
+            addRole();
+            break;
 
+            case "Add new Department":
+                addDepartment();
+                break;
+  
+          case "End":
+            connection.end();
+            break;
+        }
+});
 }
