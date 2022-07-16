5a14a9a9cbe714cbf712c05ddebdd90f2e3dbbbd
Matthew <mwhiteman83@gmail.com>
2022-07-16 17:05:10 -0600

    added functions

--- server.js
+++ server.js
@@ -72,3 +72,57 @@ console.table(res);
         firstQuestion();
     })
 }
+function viewEmployee() {
+    let dbquery="SELECT * FROM employee"
+    db.query(dbquery, function(err, res) {
+        if (err) throw err;
+console.table(res);
+        firstQuestion();
+    })
+}
+
+function viewRoles() {
+    let dbquery="SELECT * FROM role"
+    db.query(dbquery, function(err, res) {
+        if (err) throw err;
+console.table(res);
+        firstQuestion();
+    })
+}
+
+function addEmployee() {
+    let dbquery="SELECT * FROM role"
+    db.query(dbquery, function(err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type:"input", 
+                name:"firstName",
+                message:"What is the new Employee's first name?"
+            },
+            {
+                type:"input", 
+                name:"lastName",
+                message:"What is the new Employee's last name?"
+            },
+            {
+                type:"list", 
+                name:"role",
+                message:"What is the new Employee's role?",
+                choices: res.map(role => role.title)
+            },
+
+        ]).then(
+            data => {
+                let title = res.find(role => role.title===data.role)
+                let dbquery = "INSERT INTO employee SET ?"
+                db.query (dbquery, {
+                    first_name: data.firstName,
+                    last_name: data.lastName,
+                    role_id: title.id
+                })
+                firstQuestion();
+            }
+        )
+    })
+}
